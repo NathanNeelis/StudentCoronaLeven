ab5866f35f8d2da94d13e4dd2fed620060962ef9
NathanNeelis <nathan.neelis@hva.nl>
2021-01-07 12:17:57 +0100

    first commit

---
+++ src/components/intro.js
@@ -0,0 +1,14 @@
+export default function Intro (props) {
+    return (
+    <section className="introduction">
+        <div>
+            <h1 className="title">
+            Het leven van een student in corona
+            </h1>
+            <p className="intro">
+            Een groot deel van jongvolwassen konden in januari niet meer naar sportverenigingen en sportscholen. Hier vielen ook veel studenten tussen die nu thuis of in een park in de buurt moesten sporten. De meest populaire sport die niet meer beoefend kon worden was voetbal, gevolgd door trainen in de sportschool.
+            </p>
+        </div>
+    </section>
+    );
+}
