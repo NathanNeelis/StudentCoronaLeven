361cb21f37e6196bc8de0ea6ed48af538d6da673
NathanNeelis <nathan.neelis@hva.nl>
2021-01-07 15:12:13 +0100

    scrolling is working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Intro from './components/intro';
+import Sport from './components/sport';
 import './App.css';
 
 function App() {
   return (
+    <div className="scroll-container">
     <Intro />
-
+    <Sport />
+    </div>
   );
 }
 
---
+++ src/components/sport.js
@@ -0,0 +1,23 @@
+import Nathan from '../img/nathan.svg'
+import Denise from '../img/denise.svg'
+import Arthur from '../img/arthur.svg'
+import Justin from '../img/justin.svg'
+import Persona from './persona'
+
+export default function Sport (props) {
+    return (
+    <section className="sport">
+        <div>
+            <h1 className="title">
+            Het leven van een student in corona
+            </h1>
+            <p className="intro">
+            Een groot deel van jongvolwassen konden in januari niet meer naar sportverenigingen en sportscholen. Hier vielen ook veel studenten tussen die nu thuis of in een park in de buurt moesten sporten. De meest populaire sport die niet meer beoefend kon worden was voetbal, gevolgd door trainen in de sportschool.
+            </p>
+        </div>
+    </section>
+    );
+}
+
+
+// <img src={Nathan} alt='Persona Nathan' className='personaNathan' />
